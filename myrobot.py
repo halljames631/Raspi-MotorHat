da5e9ab2dc20e2255c841a9a5ef6186cbe837679
james hall <halljames631@gmail.com>
2020-04-16 12:06:46 -0400

    test code

--- myrobot.py
+++ myrobot.py
@@ -1,12 +1,39 @@
 from Raspi_MotorHAT import Raspi_MotorHAT, Raspi_DCMotor
+import time
 
 mh = Raspi_MotorHAT(addr=0x6F)
 
 leftFront = mh.getMotor(1)
 rightFront = mh.getMotor(2)
+leftRear = mh.getMotor(3)
+rightRear = mh.getMotor(4)
 
 leftFront.setSpeed(200)
 rightFront.setSpeed(200)
+leftRear.setSpeed(200)
+rightRear.setSpeed(200)
 
-leftFront.run(Raspi_MotorHAT.FORWARD)
-rightFront.run(Raspi_MotorHAT.FORWARD)
+
+def forward():
+    leftFront.run(Raspi_MotorHAT.FORWARD)
+    rightFront.run(Raspi_MotorHAT.FORWARD)
+    leftRear.run(Raspi_MotorHAT.FORWARD)
+    rightRear.run(Raspi_MotorHAT.FORWARD)
+
+def stop():
+    leftFront.run(Raspi_MotorHAT.RELEASE)
+    rightFront.run(Raspi_MotorHAT.RELEASE)
+    leftRear.run(Raspi_MotorHAT.RELEASE)
+    rightRear.run(Raspi_MotorHAT.RELEASE)
+
+i = 0
+
+while(i < 10):
+    forward()
+    time.sleep(.5)
+    i += 1
+
+stop()
+
+
+
