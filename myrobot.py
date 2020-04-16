5741ea1c24832cab441a41011ed15fae352a8b54
james hall <halljames631@gmail.com>
2020-04-16 11:53:28 -0400

    test code

--- myrobot.py
+++ myrobot.py
@@ -1,1 +1,9 @@
-import time
+from Raspi_MotorHAT import Raspi_MotorHAT, Raspi_DCMotor
+
+mh = Raspi_MotorHAT(addr=0x6F)
+
+leftFront = mh.getMotor(1)
+rightFront = mh.getMotor(2)
+
+leftFront.setSpeed(200)
+rightFront.setSpeed(200)
